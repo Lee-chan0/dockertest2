87fca5df09ee131cfda7df0686cc3b62fae75f68
Lee-chan0 <yab0403@kakao.com>
2023-12-21 01:33:34 +0900

    message

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome Docker with Express');
+});
+
+app.listen(3000, () => {
+    console.log('Server is Open');
+});
